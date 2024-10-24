2972ec1bd871d22e3c493b3dfede54a3c61b9b35
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2024-10-24 21:40:35 +0530

    Update server.js to change the default port to 5000

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import registerRoute from "./routes/codeRush/codeRushRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
